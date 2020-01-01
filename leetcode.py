ae26833036dbb1f32e09d67184771938f9ce3b69
Don Vu <dvu37@gatech.edu>
2019-12-31 16:29:18 -0800

    lofi

--- leetcode.py
+++ leetcode.py
@@ -209,20 +209,37 @@ matrix[:]=map(list,zip(*matrix[::-1]))
 #Reverse String
 #51% returns an iterator ready to traverse the list in reversed order
 s[:]=reversed(s)
-#93% does it inplace, modifies the list itself and does not return a value
+#93% does it inplace, modifies the list itself and does not return a value , only workson a list
 s.reversed()
-repeat('aabccddefg')
 #15% does not change list but returns reversed slice
 s[:]=s[::-1]
+#reverse real way 2 pts 85%
+#constant space, linear time
+        left=0
+        right=len(s)-1
+        
+        while left<right:
+            s[left],s[right]=s[right],s[left]
+            left+=1
+            right-=1
+        
 
 #Reverse Integer
 #96%
-sign = (x > 0) - (x < 0)
+#linear time because of the reverse 
+#constant space
+sign = (x > 0) - (x < 0) #alt way of findiing sign  sign = [1,-1][x < 0] cus [1,-1] is a list and use 0 or 1 to index
 nums = sign * int((str(abs(x))[::-1]))
 if -(2) ** 31 <= nums < 2 ** 31:
     return (nums)
 else:
     return (0)
+
+#bit cleaner
+        sign=(x>0)-(x<0)
+        ans=int(str(abs(x))[::-1]) #have to use [::-1] because its a string not a list,and we dont want an iterator
+        ans=sign*(ans)
+        return(ans if -2**31<ans<2**31-1 else 0)
 
 #First Unique Character in a String
 #14% -> 65%
@@ -239,8 +256,29 @@ if len(d.values()) > 0: #change to just If d: brings from 65% to 87%
 else:
     return (-1)
 
+#this way makes much faster by making seen a hash map for constant time search
+        d={}
+        seen={}
+        for ind,num in enumerate(s):
+            if num not in seen:
+                d[num]=ind
+                seen[num]=ind
+            elif num in d:
+                del d[num]
+        
+        return(-1 if len(d)==0 else min(d.values()))
+
+#or this way faster time O(N) space O(N) since gotta make two N passses and save memory for hash map
+90%
+        count = collections.Counter(s)
+        for i,n in enumerate(s):
+            if count[n]==1:
+                return(i)
+        
+        return(-1)
+
 #Valid Anagram
-# 1 dic 70%
+# 1 dic 70%, this one better than two dict becaues two dict takes 3n time and 2n space, while 1 dict takes 3nt time and 1n space
 d = {}
 for i in s:
     d[i] = d.get(i, 0) + 1
@@ -268,15 +306,32 @@ d2 = {}
 for i in t:
     d2[i] = d2.get(i, 0) + 1
 
-if d == d2:
+if d == d2: #comparting two dicts is a recursive lookup, takes O(n) fro comparison
     return (True)
 else:
     return (False)
 #Valid Palindrome
-#81% takes Big O n space
+#81% takes O(n) time  O(N) space because create temp array to hold the reverse slice array
 s = s.lower()
 s = [x for x in s if x.isalnum()]
 return (s == s[::-1])
+
+#takes 0(N) time and 0(1) space since just doing lookups/comparisons and moving pointers
+        l=0
+        r=len(s)-1
+        
+        while l<r: # has to go thru this check everytime after an event is triggered
+            if not s[l].isalnum():
+                l+=1
+            elif not s[r].isalnum():
+                r-=1
+            else:
+                if s[l].lower()==s[r].lower():
+                    l+=1    
+                    r-=1
+                else:
+                    return(False)
+        return(True)
 
 #40% takes big o constant in place
 l, r = 0, len(s) - 1
@@ -344,14 +399,14 @@ for i in range(n - 1):
 return start
 
 #Longest Common Prefix
-#73%
+#73% linear time and constant space
 if not strs:
     return ("")
 shortest = min(strs, key=len)
 for i, l in enumerate(shortest):
-    for others in strs:
+    for others in strs: #O(N here)
         if others[i] != l:
-            return (shortest[:i])
+            return (shortest[:i]) # O(1) here since input doesnt affect lenght of shortest word
 return (shortest)
 
 #delete node in linked list
@@ -378,7 +433,7 @@ slow.next = slow.next.next
 return (head)
 
 #reverse node in linked list
-#32 ms, 13.8mb , 97%
+#32 ms, 13.8mb , 97% O(L) time O(1) space, linear time cus gotta make a pass thru all # of nodes
 prev = None
 nextt = None
 curr = head
@@ -393,6 +448,7 @@ return (prev)
 
 #merge two sorted lists
 #pointer method 36ms, 12.8mb, 94%
+#O(n+m) so linear time and O(1) since only uisng a few pointers
 dummy = cur = ListNode(0)
 while l1 and l2:
     if l1.val < l2.val:
@@ -407,6 +463,7 @@ return (dummy.next)
 
 #palindrome linked list
 #56 ms, 22.8mb 99%
+#this takes O(N) time and O(1) since just using pointers
 fast = slow = head
 while fast and fast.next:#if # of nodes is even, then slow will land right at the node right after the midpoint since fast is traveling twice as fast, then you reverse the following linked list and compare it to the head list
     #goal is to get fast to the end (null) using 2x speed so just need to check fast and fast.next before advancin two nodes forward
@@ -427,8 +484,26 @@ while nd: # compare the reverse linked list and head
     head = head.next
 return (True)
 
+#alternative way 50%
+#copies the linkedlist into an array then uses two pointers to find reverse and compare
+#O(n) time for first pass to make the array and then later on for making reverse O(n) space cus making new array to hold linked list vals and also for reverse later on
+        ans=[]
+        cur=head
+        while cur!=None:
+            ans.append(cur.val)
+            cur=cur.next
+        
+        l=0
+        r=len(ans)-1
+        og=ans.copy()
+        while l<r:
+            ans[l],ans[r]=ans[r],ans[l]
+            l+=1
+            r-=1
+        return(og==ans)
 #Linked list cycle
 #48ms ,16.1 mb, 94%
+#O(N) time cus it does one pass and O(1) cus we only use two pointers
 if not head:
     return (False)
 
@@ -441,6 +516,20 @@ while slow != fast: #this part check if there is a cycle
     fast = fast.next.next #if we cant prove either then, advance both forward until we prove one
     slow = slow.next
 return (True)
+
+#50% using hash tables 
+# O(n) time  does one pass ,and O(n) space for creating the hash
+        if not head:
+            return(False)
+        
+        d={}
+        while head!=None:
+            if d.get(id(head),0)!=0:
+                return(True)
+            else:
+                d[id(head)]=1
+            head=head.next
+        return(False)
 
 #maximum depth of binary tree
 #40ms ,13.8mb, 96%
@@ -621,7 +710,7 @@ while n > 0:
 
         max_profit, min_price = 0, float('inf')
         
-        for price in prices:
+        for price in prices: #perform check to see if we shud use price as min price or to calc max profit or none
             min_price = min(min_price, price)#iterate thru and find the lowest price
             profit = price - min_price #keep trackin of profit but we only gon keep when the best one
             max_profit = max(max_profit, profit) #keep track of which day will give us the highest profit with respective to the lowest price we been tracking
@@ -754,7 +843,7 @@ while n > 0:
 
     # 96% cleaner
     return ['Fizz' * (not i % 3) + 'Buzz' * (not i % 5) or str(i) for i in range(1, n+1)]# i % 5 == 0 and not 0 is 1
-
+#choose whichever is present, if both are present then choose value on left
     #count primes
     #88%
         if n<3:
@@ -763,7 +852,7 @@ while n > 0:
     ans=[True]*n # keep it at n even tho  Q wants less than n
     ans[0]=False # becus we throwaway this 0 element
     ans[1]=False #and the index will have meaning equal the element which is good cus the index will go up to right before n 
-    for i in range(2,int(n/2)+1): # just need to iterate up to the last # whose squared value is equal to or greater than n
+    for i in range(2,int(n/2)+1): # just need to iterate up to the last # whose squared value is equal to or greater than n(ex:lowest bound is 2*2=4)
         if ans[i]:
             ans[i*i:n:i]=[False]*len(ans[i*i:n:i]) 
             
@@ -891,6 +980,7 @@ return(bin(x^y).count('1'))
     
 #missing number
 #96% using constant space and constant time
+#proofs here that the n-th Triangular number, 1+2+3+...+n is n(n+1)/2
         n = len(nums)
         return int(n * (n+1) / 2 - sum(nums))
 
